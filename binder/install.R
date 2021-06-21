974f2c3719f796d9adfed4d06b424485d5e19a6f
Alex <alex.lemenze@rutgers.edu>
2021-06-20 22:44:06 -0400

    Create install.R

---
+++ binder/install.R
@@ -0,0 +1,13 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
+
+pkgs = c("knitr", "rmarkdown", "ggplot2", "ggpubr", "reshape2","colorbrewer","tidyverse","msigdbr","plotly","stringr","venndiagram","devtools", "pheatmap")
+
+install.packages(pkgs)
+
+BiocManager::install("DESeq2")
+BiocManager::install("clusterProfiler")
+BiocManager::install("clusterProfiler")
+BiocManager::install("PCAtools")
+BiocManager::install("EnhancedVolcano")
