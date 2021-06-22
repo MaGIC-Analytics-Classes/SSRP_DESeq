5fff55e0911ba1fcc5c592d4e16e5397946fd7ec
Alex Lemenze <56348296+alemenze@users.noreply.github.com>
2021-06-21 20:36:29 -0400

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -2,10 +2,10 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install()
 
-pkgs = c('r-lib','GenomicRanges','SummarizedExperiment','genefilter','locfit','geneplotter','Hmisc','RcppArmadillo',"knitr", "remotes","rmarkdown","Biobase", "ggraph","tidygraph","ggplot2", "ggpubr", "reshape2","RColorBrewer","tidyverse","msigdbr","plotly","stringr","venndiagram","VennDiagram","devtools", "pheatmap",'IRkernel','UpSetR','scatterplot3d')
+pkgs = c("devtools","reshape2","reshape","cowplot","superheat",'r-lib','GenomicRanges','SummarizedExperiment','genefilter','locfit','geneplotter','Hmisc','RcppArmadillo',"knitr", "remotes","rmarkdown","Biobase", "ggraph","tidygraph","ggplot2", "ggpubr", "reshape2","RColorBrewer","tidyverse","msigdbr","plotly","stringr","venndiagram","VennDiagram","devtools", "pheatmap",'IRkernel','UpSetR','scatterplot3d')
 
 install.packages(pkgs)
 
-BiocManager::install(c("DESeq2","EnhancedVolcano","enrichplot","pathview","annotate","geneplotter","genefilter","clusterProfiler"))
+BiocManager::install(c("DESeq2","EnhancedVolcano","enrichplot","pathview","annotate","geneplotter","DelayedArray","genefilter","clusterProfiler"))
 remotes::install_github('kevinblighe/PCAtools')
 BiocManager::install('org.Mm.eg.db', character.only = TRUE)
